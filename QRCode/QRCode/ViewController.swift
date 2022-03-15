66cd5eac71ed502a2ee896d7699fbeb0180a7fea
gundogduonur <gundogduonur@outlook.com.tr>
2022-03-15 23:33:41 +0300

    created generate function

--- QRCode/QRCode/ViewController.swift
+++ QRCode/QRCode/ViewController.swift
@@ -13,11 +13,26 @@ class ViewController: UIViewController {
     @IBOutlet weak var titleText: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
     @IBAction func generateClicked(_ sender: Any) {
+        let title = titleText.text!
+        
     }
-    
+    func generate(Name:String) -> UIImage?
+    {
+        let data=Name.data(using: String.Encoding.ascii)
+        if let filter = CIFilter(name : "CIQRCodeGenerator")
+        {
+            filter.setValue(data, forKey: "Title")
+            let scale=CGAffineTransform(scaleX: 4, y: 4)
+            if let output=filter.outputImage?.transformed(by: scale)
+            {
+                return UIImage(ciImage: output)
+            }
+        }
+        return nil
+    }
 }
 
