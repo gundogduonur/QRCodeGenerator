01d345082102f2e894514931886558a443c23a3a
gundogduonur <gundogduonur@outlook.com.tr>
2022-03-15 23:14:45 +0300

    Defined components in storyboard

--- QRCode/QRCode/ViewController.swift
+++ QRCode/QRCode/ViewController.swift
@@ -9,11 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var qrImage: UIImageView!
+    @IBOutlet weak var titleText: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func generateClicked(_ sender: Any) {
+    }
+    
 }
 
